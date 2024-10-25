e7a6cfa6b3cdd2497edb34234ea3fcf407713b84
Eduardo_465 <20221076@uthh.edu.mx>
2024-10-25 10:22:29 -0600

    conectando a la api de render

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,8 @@ export const environment = {
   production: false,
   // api: 'https://purificadoraback.onrender.com',
   // https://proyecto-atr-backend.onrender.com/api/v1/usuarios
-  api: 'http://localhost:4000/api/v1', //?Local
+
+  api: 'https://proyecto-atr-backend.onrender.com/api/v1',
+  // api: 'http://localhost:4000/api/v1', //?Local
   keyCapchat: '6LfdbGcqAAAAAEtcCSBTxBMZmBIFO5gIIH_iDzMc', //?Local
 };
