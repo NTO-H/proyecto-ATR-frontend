73cfb23a61b2235fd595f69e8b254067b5c9a475
anthony= <134122438+Nikrovikey@users.noreply.github.com>
2024-11-10 19:51:41 -0600

    subir

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -19,6 +19,9 @@ export const appConfig: ApplicationConfig = {
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000',
-    }),
+    }), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
   ],
 };
