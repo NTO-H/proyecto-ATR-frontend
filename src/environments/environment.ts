df00b09e438de91247510a1678918805f89fed9b
Antonius <134122438+NTO-H@users.noreply.github.com>
2024-12-09 14:34:37 -0600

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: false,
-  // api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
-  api: 'http://localhost:4000/api/v1', //!Local
+  
+  api: 'https://proyecto-atr-backend.onrender.com/api/v1', //!alojado
+  //api: 'http://localhost:4000/api/v1', //!Local
   serviceWorker: true,
   // keyCapchat: '6LfdbGcqAAAAAEtcCSBTxBMZmBIFO5gIIH_iDzMc', //?Local
 };
